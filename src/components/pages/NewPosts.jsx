7d4f3e0548abdd0f61bd79a8c58de64cd5c66e00
WisdomPandovic <wisdomokezieonwuchekwa@gmail.com>
2025-05-09 13:25:56 +0100

    L

--- src/components/pages/NewPosts.jsx
+++ src/components/pages/NewPosts.jsx
@@ -1,306 +1,3 @@
-// import React, { useState, useContext, useEffect } from 'react';
-// import { ImgurContext } from '../Context/ImgurContext';
-// import { BsArrowLeftShort } from 'react-icons/bs';
-// import { Link } from 'react-router-dom';
-// import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import { ToastContainer } from 'react-toastify';
-// import { storage } from '../../firebase'; // Import Firebase storage
-// import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
-
-// function NewPosts() {
-//   const [title, setTitle] = useState('');
-//   const [description, setDescription] = useState('');
-//   const [selectedTag, setSelectedTag] = useState('');
-//   const [newTag, setNewTag] = useState('');
-//   const [tags, setTags] = useState([]);
-//   const [imageFile, setImageFile] = useState(null);
-//   const [imagePreview, setImagePreview] = useState('');
-//   const [imageURL, setImageURL] = useState('');
-//   const { userID } = useContext(ImgurContext);
-
-//   useEffect(() => {
-//     fetchTags();
-//   }, []);
-
-//   const fetchTags = async () => {
-//     try {
-//       const response = await fetch('https://imgurif-api.onrender.com/api/tag');
-//       const fetchedTags = await response.json();
-//       setTags(fetchedTags);
-//     } catch (error) {
-//       console.error('Error fetching tags:', error);
-//     }
-//   };
-
-//   const handleInputChange = (event) => {
-//     const { name, value } = event.target;
-//     switch (name) {
-//       case 'title':
-//         setTitle(value);
-//         break;
-//       case 'description':
-//         setDescription(value);
-//         break;
-//       case 'selectedTag':
-//         setSelectedTag(value);
-//         setNewTag('');
-//         break;
-//       case 'newTag':
-//         setNewTag(value);
-//         break;
-//       case 'image':
-//         const file = event.target.files[0];
-//         setImageFile(file);
-//         setImagePreview(URL.createObjectURL(file));
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   const validateImage = (file) => {
-//     const supportedTypes = ['image/jpeg', 'image/png'];
-//     return supportedTypes.includes(file.type);
-//   };
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-
-//     if (!imageFile) {
-//       toast.error('Please select an image file');
-//       return;
-//     }
-
-//     if (!validateImage(imageFile)) {
-//       toast.error('Invalid image format. Only JPEG and PNG allowed');
-//       return;
-//     }
-
-//     try {
-//       // Upload image to Firebase Storage
-//       const storageRef = ref(storage, `images/${imageFile.name}`);
-//       const uploadTask = uploadBytesResumable(storageRef, imageFile);
-
-//       uploadTask.on(
-//         'state_changed',
-//         (snapshot) => {
-//           const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//           console.log(`Upload is ${progress}% done`);
-//           toast.info(`Uploading image: ${Math.round(progress)}%`);
-//         },
-//         (error) => {
-//           console.error('Error uploading file:', error);
-//           toast.error('Error uploading image');
-//         },
-//         async () => {
-//           // Get download URL after successful upload
-//           const downloadURL = await getDownloadURL(storageRef);
-//           setImageURL(downloadURL); // Set image URL
-
-//           let tagData;
-//           if (selectedTag === 'new') {
-//             const response = await fetch('https://imgurif-api.onrender.com/api/tag', {
-//               method: 'POST',
-//               headers: {
-//                 'Content-Type': 'application/json',
-//               },
-//               body: JSON.stringify({ name: newTag }),
-//             });
-//             const newTagData = await response.json();
-//             tagData = { _id: newTagData._id, name: newTag };
-//           } else {
-//             const selectedTagData = JSON.parse(selectedTag);
-//             tagData = { _id: selectedTagData._id, name: selectedTagData.name };
-//           }
-
-//           // Create post
-//           const formData = new FormData();
-//           formData.append('title', title);
-//           formData.append('description', description);
-//           formData.append('tag', tagData.name);
-//           formData.append('imagepath', downloadURL);
-//           formData.append('user', userID);
-
-//           const response = await fetch('https://imgurif-api.onrender.com/api/post', {
-//             method: 'POST',
-//             body: formData,
-//           });
-
-//           if (response.ok) {
-//             toast.success('Post created successfully');
-//             setTitle('');
-//             setDescription('');
-//             setSelectedTag('');
-//             setNewTag('');
-//             setImageFile(null);
-//             setImagePreview('');
-//           } else {
-//             toast.error('Failed to create post');
-//           }
-//         }
-//       );
-//     } catch (error) {
-//       console.error('Error creating post:', error);
-//       toast.error('An error occurred during post creation');
-//     }
-//   };
-
-
-//   return (
-//     <div className="post-bk">
-//       <div className="back pt-3 pb-3">
-//         <Link to="/" className="td">
-//           <BsArrowLeftShort /> back to Imgur
-//         </Link>
-//       </div>
-//       <div className="container pt-5 pb-5">
-//         <div className="row">
-//           <div className="">
-//             <div className="">
-//               {/* <div className="card-header">
-//                 <h2 className="text-center text-dark">Create New Post</h2>
-//               </div> */}
-//               <div className="card-body text-dark">
-//                 <form onSubmit={handleSubmit} className="create-post-form">
-//                   <div className="row">
-//                     <div className="col-lg-6">
-//                       <div className="form-group">
-//                         <input
-//                           type="text"
-//                           className="form-control title"
-//                           id="title"
-//                           name="title"
-//                           placeholder="Give your post a unique title..."
-//                           value={title}
-//                           onChange={handleInputChange}
-//                           required
-//                         />
-//                       </div>
-//                       {/* <div className="form-group mt-3">
-//                         <label htmlFor="image">Image</label>
-//                         <input
-//                           type="file"
-//                           className="form-control-file"
-//                           id="image"
-//                           name="image"
-//                           onChange={handleInputChange}
-//                           required
-//                         />
-//                         {imagePreview && (
-//                           <img
-//                             src={imagePreview}
-//                             alt="Selected"
-//                             style={{ marginTop: '10px', maxWidth: '100%' }}
-//                           />
-//                         )}
-//                       </div> */}
-//                       <div className="form-group mt-3">
-//                         <label
-//                           htmlFor="image"
-//                           className="btn btn-outline-primary w-100 d-flex justify-content-center align-items-center"
-//                           style={{
-//                             height: '50px',
-//                             border: '2px solid #007bff',
-//                             cursor: 'pointer',
-//                             color: 'white'
-//                           }}
-//                         >
-//                           Upload Image
-//                         </label>
-//                         <input
-//                           type="file"
-//                           className="form-control-file"
-//                           id="image"
-//                           name="image"
-//                           onChange={handleInputChange}
-//                           required
-//                           style={{ display: 'none' }} // Hide the default input
-//                         />
-//                         {imagePreview && (
-//                           <img
-//                             src={imagePreview}
-//                             alt="Selected"
-//                             style={{ marginTop: '10px', maxWidth: '100%' }}
-//                           />
-//                         )}
-//                       </div>
-
-//                       <div className="form-group mt-2">
-//                         <textarea
-//                           className="form-control description"
-//                           id="description"
-//                           name="description"
-//                           placeholder="Add a description"
-//                           rows="6"
-//                           value={description}
-//                           onChange={handleInputChange}
-//                           required
-//                         />
-//                       </div>
-//                     </div>
-//                     <div className="col-lg-6">
-//                       <div className="d-flex post-buttons container">
-//                         <button type="submit" className="btn btn-primary mt-3">
-//                           To Community
-//                         </button>
-//                         <button className="btn btn-success mt-3">Grab Link</button>
-//                       </div>
-//                       <p className="pt-3">
-//                         Your post is currently <span className="text-info">Hidden</span>
-//                       </p>
-//                       <div className="form-group">
-//                         <label htmlFor="selectedTag">Tags</label>
-//                         <select
-//                           className="form-control tags"
-//                           id="selectedTag"
-//                           name="selectedTag"
-//                           value={selectedTag}
-//                           onChange={handleInputChange}
-//                         >
-//                           <option value="" disabled>
-//                             Select or Create Tag
-//                           </option>
-//                           {tags.map((tag) => (
-//                             <option key={tag._id} value={JSON.stringify(tag)}>
-//                               {tag.name}
-//                             </option>
-//                           ))}
-//                           <option value="new">Create New Tag</option>
-//                         </select>
-//                         {selectedTag === 'new' && (
-//                           <div className="mt-2">
-//                             <input
-//                               type="text"
-//                               className="form-control tags"
-//                               id="newTag"
-//                               name="newTag"
-//                               value={newTag}
-//                               onChange={handleInputChange}
-//                               placeholder="Enter New Tag Name"
-//                               required
-//                             />
-//                           </div>
-//                         )}
-//                       </div>
-//                     </div>
-//                     <div className="card-footer text-center bg-light mt-2">
-//                       <small className="text-muted">Make sure to review your post before submitting.</small>
-//                     </div>
-//                   </div>
-//                   <ToastContainer />
-//                 </form>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default NewPosts;
-
 import React, { useState, useContext, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { ImgurContext } from '../Context/ImgurContext';
@@ -311,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { storage } from '../../firebase';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { FaFacebookF, FaTwitter, FaWhatsapp, FaLinkedinIn } from 'react-icons/fa';
 
 function NewPosts() {
   const [title, setTitle] = useState('');
@@ -525,7 +223,7 @@ function NewPosts() {
                     right: isMobile ? 'auto' : '10px',
                     zIndex: isMobile ? 'auto' : '999',
                   }}>
-                    <p className="mt-3 text-white">POST</p>
+                    <p className="mt-3 text-white">Share With Community</p>
                     <div className="d-flex flex-column post-buttons">
                       <button
                         type="submit"
@@ -542,9 +240,54 @@ function NewPosts() {
                         Grab Link
                       </button>
                     </div>
-                    <p className="pt-3" style={{ color: '#b4b9c2', fontSize: "13px" }}>
+
+                    <div className="mt-5">
+                      <h2 className="text-white fs-5">Quick share</h2>
+                      <div className="d-flex align-items-center justify-content-center gap-3 mt-3">
+                        <a
+                          href="https://www.facebook.com/sharer/sharer.php?u=YOUR_URL"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="d-flex align-items-center justify-content-center rounded-circle text-white"
+                          style={{ backgroundColor: '#3b5998', width: 40, height: 40 }}
+                        >
+                          <FaFacebookF />
+                        </a>
+
+                        <a
+                          href="https://twitter.com/intent/tweet?url=YOUR_URL"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="d-flex align-items-center justify-content-center rounded-circle text-white"
+                          style={{ backgroundColor: '#1DA1F2', width: 40, height: 40 }}
+                        >
+                          <FaTwitter />
+                        </a>
+
+                        <a
+                          href="https://api.whatsapp.com/send?text=YOUR_URL"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="d-flex align-items-center justify-content-center rounded-circle text-white"
+                          style={{ backgroundColor: '#25D366', width: 40, height: 40 }}
+                        >
+                          <FaWhatsapp />
+                        </a>
+
+                        <a
+                          href="https://www.linkedin.com/shareArticle?url=YOUR_URL"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="d-flex align-items-center justify-content-center rounded-circle text-white"
+                          style={{ backgroundColor: '#0077B5', width: 40, height: 40 }}
+                        >
+                          <FaLinkedinIn />
+                        </a>
+                      </div>
+                    </div>
+                    {/* <p className="pt-3" style={{ color: '#b4b9c2', fontSize: "13px" }}>
                       Your post is currently <span className="text-success">Hidden</span>
-                    </p>
+                    </p> */}
 
                     <div className='pb-1' style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
                       <input type="checkbox" id="mature" name="mature" checked={isMature} onChange={() => setIsMature(!isMature)} />
@@ -553,7 +296,7 @@ function NewPosts() {
                       </label>
                     </div>
 
-                    <div className="form-group text-white">
+                    {/* <div className="form-group text-white">
                       <label htmlFor="selectedTag" className="mb-1">Tags</label>
                       <select
                         className="form-control tags"
@@ -587,7 +330,9 @@ function NewPosts() {
                           />
                         </div>
                       )}
-                    </div>
+                    </div> */}
+
+                    
                   </div>
                 </div>
                 <ToastContainer />
@@ -595,9 +340,9 @@ function NewPosts() {
             </div>
           </div>
         </div>
-        <div className="container d-flex justify-content-center flex-column card-footer text-center bg-light mt-5" style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+        {/* <div className="container d-flex justify-content-center flex-column card-footer text-center bg-light mt-5" style={{ position: 'absolute', bottom: 0, width: '100%' }}>
           <small className="text-muted">Make sure to review your post before submitting.</small>
-        </div>
+        </div> */}
       </div>
 
     </div>
